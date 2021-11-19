14a642f0d668236a6a648fcb16ef6828c2356a5d
AlexAguado24 <aaguado131@gmail.com>
2021-11-19 12:03:09 +0100

    filas y columnas

--- Tema 4/T04_Arrays/src/Multidimensionales/Multidimensionales.java
+++ Tema 4/T04_Arrays/src/Multidimensionales/Multidimensionales.java
@@ -7,12 +7,29 @@ public class Multidimensionales {
         arrayMulti[2][1]=8;
         arrayMulti[1][2]=6;
 
+        int[][] arrayMultiDiferente  = {{1,2,3,4},{1,2,3},{1,2}};
+
+        for (int i = 0; i < arrayMultiDiferente.length; i++) {
+            for (int j = 0; j < arrayMultiDiferente.length; j++) {
+
+            }
+        }
+
+        //Rellenar arrays con mismas filas y columnas
+        /*for (int i = 0; i < arrayMulti.length; i++) {
+            //System.out.println(arrayMulti[i]);
+            //estando en la fila
+            for (int j = 0; j < arrayMulti.length; j++) {
+                System.out.print(arrayMulti[i][j]+" ");
+            }
+            System.out.println();
+        }
         for (int i = 0; i < arrayMulti.length; i++) {
             for (int j = 0; j < arrayMulti.length; j++) {
                 System.out.print(arrayMulti[i][j]);
             }
             System.out.printf("%n");
-        }
+        }*/
 
     }
 }
