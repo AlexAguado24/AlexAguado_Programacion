989273c20b909aaf02e8b32ec245b6f87f5158b5
AlexAguado24 <aaguado131@gmail.com>
2021-12-12 18:58:21 +0100

    .

---
+++ Tema 4/T04_Arrays/src/Colecciones/Ejercicio6.java
@@ -0,0 +1,9 @@
+package Colecciones;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+
+    }
+}
+//Crea un programa que permita meter una lista de coches, donde cada coche consta de marca (String), modelo (String) y caballos (int).
+// Mete tres coches manualmente
