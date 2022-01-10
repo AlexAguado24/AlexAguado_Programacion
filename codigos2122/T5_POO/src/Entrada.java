18a6e931f9c742abf6402f819a0a0f360c065903
AlexAguado24 <aaguado131@gmail.com>
2022-01-10 11:21:09 +0100

    POO clase 1

---
+++ codigos2122/T5_POO/src/Entrada.java
@@ -0,0 +1,29 @@
+public class Entrada {
+    //ambito de clase
+    String nombre = "Alex";
+
+    public static void main(String[] args) {
+        // ambito de metodo - local
+        //String nombre = "Alex";
+        Usuario usuario1 = new Usuario();
+        // nombre="Por defecto", pss=null, correo=""Por defecto"", edad=0, fecha=null
+
+        Usuario usuario2 = new Usuario();
+        // nombre=null, pss=null, correo=null, edad=0, fecha=null
+
+        Usuario usuario3 = new Usuario("Alex", "alex.aguad@gmail.es"
+                ,"pass");
+        // nombre="Alex", pss="pass", correo="alex.aguado@gmail.es", edad=0, fecha=null
+
+        Usuario usuario4 = new Usuario("alex.aguad@gmail.es","pass",12);
+        // nombre="null", pss="pass", correo="alex.aguado@gmail.es", edad=12, fecha=null
+
+
+        /*usuario1.password = "Contraseña";
+        // nombre="contraseña", pss=null, correo=null, edad=0, fecha=null
+        System.out.println(usuario1.password);*/
+
+        System.out.println(usuario1.getPassword());
+
+    }
+}
---
+++ codigos2122/T5_POO/src/Usuario.java
@@ -0,0 +1,44 @@
+public class Usuario {
+
+    // variables
+    private String correoElectronico, nombre, aèllidos, password, fecha;
+    private int edad;
+
+
+    // costructores
+    // si no hay constructores escritos hay un contructor que es el vacio (por defecto)
+    // este contructor deja de existir cuando se escribe algun contructor
+
+    public Usuario(){
+        nombre = "Por defecto";
+        correoElectronico = "Por defecto";
+        edad = 18;
+    }
+    public Usuario(String nombre, String correoElectronico, String password){
+        this.nombre = nombre;
+        this.correoElectronico = correoElectronico;
+        this.password = password;
+    }
+    public Usuario( String correoElectronico, String password, int edad){
+        this.correoElectronico = correoElectronico;
+        this.password = password;
+        this.edad = edad;
+    }
+
+
+    // metodos
+
+    // metodos getters y setters
+
+    //getter de pass
+    public String getPassword(){
+        return password;
+    }
+
+    //setter del pass
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+
+}
