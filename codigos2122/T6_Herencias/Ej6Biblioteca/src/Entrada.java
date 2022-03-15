8cb5f20959dd149654f3234f1509292c8a8164a5
AlexAguado24 <aaguado131@gmail.com>
2022-03-15 18:53:41 +0100

    .

--- codigos2122/T6_Herencias/Ej6Biblioteca/src/Entrada.java
+++ codigos2122/T6_Herencias/Ej6Biblioteca/src/Entrada.java
@@ -3,8 +3,11 @@ public class Entrada {
     public static void main(String[] args) {
         Libro libro = new Libro();
         DVD dvd = new DVD();
-        Persona socio = new Socio("sadf","sdfd","sadf",0);
-        
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.addPersona(new Socio("aeef","qwer","fdhg",0));
+
+
+
 
     }
 
