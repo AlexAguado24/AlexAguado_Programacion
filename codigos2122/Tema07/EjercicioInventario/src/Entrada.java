b6fee8729eadfe2cd86c8f36229fb874316cc1ca
AlexAguado24 <aaguado131@gmail.com>
2022-03-25 09:26:56 +0100

    .

--- codigos2122/Tema07/EjercicioInventario/src/Entrada.java
+++ codigos2122/Tema07/EjercicioInventario/src/Entrada.java
@@ -5,7 +5,12 @@ public class Entrada {
         SupermercadoEspecifico<Alimento> supermercadoAlimentos = new SupermercadoEspecifico<Alimento>();
         supermercadoAlimentos.registrarElemento(new Carne("media","españa",50,100));
 
+
+        supermercadoAlimentos.mostrarPreciosFinales();
+
         SupermercadoEspecifico<Mueble> supermercadoMuebles = new SupermercadoEspecifico<Mueble>();
+        supermercadoMuebles.registrarElemento(new Mesa("madera","35kg",100,20));
+
 
     }
 
