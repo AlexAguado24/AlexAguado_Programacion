78c380d65f904f9664170079bd35005cbf6247e5
AlexAguado24 <aaguado131@gmail.com>
2022-04-24 19:43:16 +0200

    .

--- codigos2122/Tema08/Codigos2122/UsuarioRecurrente/src/Entrada.java
+++ codigos2122/Tema08/Codigos2122/UsuarioRecurrente/src/Entrada.java
@@ -1,5 +1,39 @@
+import controller.UsuariosController;
+import model.Usuario;
+
+import java.util.Scanner;
+
 public class Entrada {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        UsuariosController usuariosController = new UsuariosController();
+
+        String nombre, apellido, pass;
+        int opcionUsuario = 0, opcionLectura = 0;
+
+        do {
+            System.out.println("Nombre:");
+            nombre = in.next();
+            System.out.println("Apellido:");
+            apellido = in.next();
+            System.out.println("Password:");
+            pass = in.next();
+            Usuario usuario = new Usuario(nombre,apellido,pass);
+            usuariosController.addUsuario(usuario);
+            System.out.println("¿Quierers introducir otro usuario (0- no/ 1- si)?");
+            opcionUsuario = in.nextInt();
+        } while (opcionUsuario != 0);
+
+        usuariosController.escribirUsuario();
+
+        System.out.println("Quieres leer los ficheros");
+        opcionLectura = in.nextInt();
+
+        if (opcionLectura == 1){
+            usuariosController.lecturaUsuario();
+        }
+
 
     }
 }
--- codigos2122/Tema08/Codigos2122/UsuarioRecurrente/src/controller/UsuariosController.java
+++ codigos2122/Tema08/Codigos2122/UsuarioRecurrente/src/controller/UsuariosController.java
@@ -3,42 +3,71 @@ package controller;
 import model.Usuario;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UsuariosController {
-    public void escribirUsuarios () {
-        Scanner in = new Scanner(System.in);
-        File file = new File("src/resources/usuario_ejercicio.bin");
-        //Cerrarlo
-        ObjectOutputStream oos = null;
-        int opcion = 0;
-        Usuario usuario = new Usuario();
-        do {
+    ArrayList<Usuario> listaUsuarios;
 
+    public UsuariosController() {
+        listaUsuarios = new ArrayList<>();
+    }
+
+    public void addUsuario(Usuario usuario) {
+        listaUsuarios.add(usuario);
+    }
+
+    public void escribirUsuario() {
+        File file = new File("src/resources/usuario.bin");
+        ObjectOutputStream oop = null;
+
+        try {
+            oop = new ObjectOutputStream(new FileOutputStream(file));
+            for (Usuario item : listaUsuarios) {
+                oop.writeObject(item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
             try {
-                oos = new ObjectOutputStream(new FileOutputStream(file));
-                System.out.println("Introduce los datos del usuario");
-                System.out.println("Nombre");
-                usuario.setNombre(in.next());
-                System.out.println("Apellido");
-                usuario.setApellido(in.next());
-                System.out.println("Password");
-                usuario.setPassword(in.next());
-                oos.writeObject(usuario);
+                if (oop != null) {
+                    oop.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                try {
-                    if (oos != null) {
-                        oos.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
-            System.out.println("¿Quieres añadir otro usuario? (1- si/ 2- no)");
-            opcion = in.nextInt();
-        } while (opcion != 2);
+        }
+    }
 
+    public void mostrarDatosUsuario(Usuario usuario) {
+        System.out.println("Nombre: " + usuario.getNombre());
+        System.out.println("Apellido: " + usuario.getApellido());
+    }
+
+    public void lecturaUsuario() {
+        File file = new File("src/resources/usuario.bin");
+        ObjectInputStream oip = null;
+
+        try {
+            oip = new ObjectInputStream(new FileInputStream(file));
+            Usuario item;
+            try {
+                while ((item = (Usuario) oip.readObject())!= null){
+                    mostrarDatosUsuario(item);
+                }
+            } catch (EOFException e){
+                System.out.println("Fichero terminado");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (oip != null) {
+                    oip.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
