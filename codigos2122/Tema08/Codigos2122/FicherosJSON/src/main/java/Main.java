5839afdb530436f7f53766684a2f36a630cb65a6
AlexAguado24 <aaguado131@gmail.com>
2022-05-04 19:19:48 +0200

    .

--- codigos2122/Tema08/Codigos2122/FicherosJSON/src/main/java/Main.java
+++ codigos2122/Tema08/Codigos2122/FicherosJSON/src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         JSONController controller = new JSONController();
-        controller.leerFicheroJSON();
+        //controller.leerFicheroJSON();
     }
 }
--- codigos2122/Tema08/Codigos2122/FicherosJSON/src/main/java/controller/JSONController.java
+++ codigos2122/Tema08/Codigos2122/FicherosJSON/src/main/java/controller/JSONController.java
@@ -9,36 +9,6 @@ import java.io.*;
 
 public class JSONController {
 
-    String jsonString = "{\n" +
-            "  \"nombre\": \"Borja\",\n" +
-            "  \"apellidos\": \"Martin Herrera\",\n" +
-            "  \"edad\": 38,\n" +
-            "  \"hobbies\": [\n" +
-            "    \"musica\",\n" +
-            "    \"deporte\",\n" +
-            "    \"cine\",\n" +
-            "    \"informática\"\n" +
-            "  ]}";
-
-    public void pasarStringJSON() {
-
-        JSONObject jsonObject = new JSONObject(jsonString);
-        String nombre = jsonObject.getString("nombre");
-        int edad = jsonObject.getInt("edad");
-        JSONArray hobbies = jsonObject.getJSONArray("hobbies");
-
-
-        System.out.println(nombre);
-        System.out.println(edad);
-
-        System.out.println("Tus hobbies son");
-        for (int i = 0; i < hobbies.length(); i++) {
-            System.out.println(hobbies.getString(i));
-        }
-
-
-    }
-
     public void leerFicheroJSON() {
         File file = new File("src/main/resources/Persona.json");
 
@@ -94,6 +64,38 @@ public class JSONController {
         }
 
     }
+    public void leerJSONAsignaturas(){
+        File file = new File("src/main/resources/asignatura.json");
 
+        BufferedReader br = null;
+        String linea = null;
+        StringBuffer lineaCompleta = new StringBuffer();
+
+        try {
+            br = new BufferedReader(new FileReader(file));
+            while ((linea = br.readLine())!=null){
+                lineaCompleta.append(linea);
+            }
+
+            JSONObject jsonAsignaturas = new JSONObject(lineaCompleta.toString());
+
+            JSONArray asignaturas = jsonAsignaturas.getJSONArray("asignaturas");
+
+            
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 }
