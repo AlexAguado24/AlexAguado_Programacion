2f7947b51eb83c809747ae5ae98c68507a1581a8
AlexAguado24 <aaguado131@gmail.com>
2022-05-04 17:33:46 +0200

    .

--- codigos2122/Tema08/Codigos2122/FicherosBinarios/src/Entrada.java
+++ codigos2122/Tema08/Codigos2122/FicherosBinarios/src/Entrada.java
@@ -4,7 +4,6 @@ public class Entrada {
     public static void main(String[] args) {
         BinariosController controller = new BinariosController();
         //controller.escribirBinario();
-        controller.lecturaBinario();
 
     }
 }
